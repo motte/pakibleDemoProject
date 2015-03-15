4a977cfadceac174648b3bdbcd255d736c56228c
motte <michaelotte1@gmail.com>
2015-03-15 13:31:55 -0700

    [MO] Getting started on front-end

--- pakibledemo/apps/preview/views.py
+++ pakibledemo/apps/preview/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class DemoTemplateView(TemplateView):
+    template_name = 'demo.html'
